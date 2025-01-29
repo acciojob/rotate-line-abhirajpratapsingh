25c63d09623328fa52844c68be9fc63d524e172c
abhiraj pratap singh <112178869+abhirajpratapsingh@users.noreply.github.com>
2025-01-29 07:50:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let angle = 0;
+
+function rotateLine() {
+  angle += 2;
+  document.getElementById('line').style.transform = `rotate(${angle}deg)`;
+}
+
+setInterval(rotateLine, 20);
